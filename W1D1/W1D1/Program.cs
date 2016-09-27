54d974c17d7f6ed574228eeb05e12099acc51402
ellis <ellis@DESKTOP-V74PEUB>
2016-09-26 17:38:01 -0700

    assignment

---
+++ W1D1/W1D1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1D1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            int myInteger = 1492;
+            bool learningBools = true;
+            DateTime myString2 = Convert.ToDateTime("9/26/2016");
+            double myDouble = 5.45;
+            string myString1 = "X";
+            string myString = "Hello, World";
+
+            Console.WriteLine("Columbus sailed the world in " + myInteger);
+            Console.ReadLine();
+        }
+    }
+}
